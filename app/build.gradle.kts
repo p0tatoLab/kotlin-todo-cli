f253a98064404e1fb2c66772f987befa272a1f13
Yutaro Kondo <konkon.ponpon1130@gmail.com>
2025-11-09 02:34:36 +0900

    chore: プロジェクト初期セットアップ

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+android {
+    namespace = "com.example.todocli"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.todocli"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    // Kotlinx Serialization (JSON処理)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    testImplementation("junit:junit:4.13.2")
+}
